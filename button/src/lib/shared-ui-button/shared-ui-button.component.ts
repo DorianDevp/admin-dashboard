d4bfa379674b4a6ff6050b69fbe9968b2028a6ca
Dorian Donoch <dorian.donoch@ventia.pl>
2024-09-23 16:46:19 +0200

    squash! wip: converting monolith to libs

---
+++ button/src/lib/shared-ui-button/shared-ui-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'vcrm-shared-ui-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shared-ui-button.component.html',
+  styleUrl: './shared-ui-button.component.scss',
+})
+export class SharedUiButtonComponent {}
